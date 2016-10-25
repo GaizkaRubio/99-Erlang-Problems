5ca71bcf8b1896596bac9699a6bd5bbd9e48a9d3
Gaizka Rubio Landaluze <grubio@stratio.com>
2016-10-25 08:49:04 +0200

    git ignore file

---
+++ src/eleventoTwentyExerTest.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%%% @author grubio
+%%% @copyright (C) 2016, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 24. oct 2016 10:38
+%%%-------------------------------------------------------------------
+-module(eleventoTwentyExerTest).
+-author("grubio").
+
+-include_lib("eunit/include/eunit.hrl").
+
+exercise11_test() ->
+  ?assert(eleventoTwentyExercises:decodeModified([b,[5,a],d,[3,c]])==[b,a,a,a,a,a,d,c,c,c]).
+
+exercise14_test() ->
+  ?assert(eleventoTwentyExercises:dupli([a,b,c,c,d])==[a,a,b,b,c,c,c,c,c,d,d]).
---
+++ src/eleventoTwentyExercises.erl
@@ -0,0 +1,32 @@
+%%%-------------------------------------------------------------------
+%%% @author grubio
+%%% @copyright (C) 2016, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 24. oct 2016 8:31
+%%%-------------------------------------------------------------------
+-module(eleventoTwentyExercises).
+-author("grubio").
+
+%% API
+-export([decodeModified/1, dupli/1, repli/2]).
+
+%%Problem 12 - Decode a run-length encoded list.
+decodeModified(L) -> decodeModified([],L).
+decodeModified(NL, [[X|Y]|T]) -> decodeModified(NL++[Y || _ <- lists:seq(1, X)],T);
+decodeModified(NL, [H|T]) -> decodeModified(NL++[H],T);
+decodeModified(NL, []) -> lists:flatten(NL).
+
+%%Problem 13 - Run-length encoding of a list (direct solution).
+encodeDirect([H|T]) -> H.
+
+%%Problem 14 - Duplicate the elements of a list.
+dupli(L) -> dupli([], L).
+dupli(NL, [H|T]) -> dupli(NL++[H || _ <- lists:seq(1, 2)], T);
+dupli(NL, []) -> lists:flatten(NL).
+
+%%Problem 15 - Replicate the elements of a list a given number of times.
+repli(L, N) -> repli([], L, N).
+repli(NL, [H|T], N) -> repli(NL++[H || _ <- lists:seq(1, N)], T, N);
+repli(NL, [], _) -> lists:flatten(NL).
