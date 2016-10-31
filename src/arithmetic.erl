82d2caddeb13be933cb8d2038d9c0fd5f1033e87
Gaizka Rubio Landaluze <grubio@stratio.com>
2016-10-31 17:27:36 +0100

    arithmetic exercises

--- src/arithmetic.erl
+++ src/arithmetic.erl
@@ -10,7 +10,7 @@
 -author("grubio").
 
 %% API
--export([is_prime/1, gcd/2]).
+-export([is_prime/1, gcd/2, coprime/2]).
 
 %%Problem 31 - Determine whether a given integer number is prime.
 is_prime(N) -> is_prime(N, N-1).
@@ -18,9 +18,10 @@ is_prime(_, 1) -> true;
 is_prime(N, D) when N rem D =:= 0 -> false;
 is_prime(N, D) when N rem D =/= 0 -> is_prime(N, D-1).
 
-
 %%Problem 32 - Determine the greatest common divisor of two positive integer numbers.
 gcd(N, D) when N rem D =:= 0 -> D;
 gcd(N, D) when N rem D =/= 0, N rem D > D  -> gcd(N, D);
 gcd(N, D) when N rem D =/= 0 -> gcd(N, N rem D).
 
+%%Problem 33 -  Determine whether two positive integer numbers are coprime.
+coprime(N, D) -> gcd(N, D) == 1.
--- src/arithmeticTest.erl
+++ src/arithmeticTest.erl
@@ -12,10 +12,12 @@
 -include_lib("eunit/include/eunit.hrl").
 
 exercise31_test() ->
-%%  io:fwrite("~w", [twentyOnetoTwentyEightExercises:insert_at(alfa,[a,b,c,d],2)]),
+%%  io:fwrite("~w", [arithmetic:is_prime(5)]),
   ?assert(arithmetic:is_prime(5)),
   ?assertNot(arithmetic:is_prime(4)).
 
 exercise32_test() ->
-%%  io:fwrite("~w", [twentyOnetoTwentyEightExercises:insert_at(alfa,[a,b,c,d],2)]),
-  ?assert(arithmetic:gcd(36,63)==9).
+  ?assert(arithmetic:gcd(63,36)==9).
+
+exercise33_test() ->
+  ?assert(arithmetic:gcd(64,35)).
